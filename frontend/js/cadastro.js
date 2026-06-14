4a2d98c081f9dcfc32866fbd8cf0010f06bfc07c
jadielalves <jadielweb@gmail.com>
2026-06-14 20:18:55 -0300

    categorias

--- frontend/js/cadastro.js
+++ frontend/js/cadastro.js
@@ -1,4 +1,71 @@
 console.log("JS carregado!");
+
+const campoSenha =
+    document.getElementById("senha");
+
+const campoConfirmarSenha =
+    document.getElementById("confirmarsenha");
+
+const botaoSenha =
+    document.getElementById("mostrarsenha");
+
+const botaoConfirmarSenha =
+    document.getElementById("mostrarsenha2");
+
+const olhoAberto = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+viewBox="0 0 24 24" fill="none" stroke="currentColor"
+stroke-width="2" stroke-linecap="round"
+stroke-linejoin="round">
+    <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/>
+    <circle cx="12" cy="12" r="3"/>
+</svg>
+`;
+
+const olhoFechado = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+viewBox="0 0 24 24" fill="none" stroke="currentColor"
+stroke-width="2" stroke-linecap="round"
+stroke-linejoin="round">
+    <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575"/>
+    <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/>
+    <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151"/>
+    <path d="m2 2 20 20"/>
+</svg>
+`;
+
+botaoSenha.addEventListener("click", () => {
+
+    if (campoSenha.type === "password") {
+
+        campoSenha.type = "text";
+        botaoSenha.innerHTML = olhoAberto;
+
+    } else {
+
+        campoSenha.type = "password";
+        botaoSenha.innerHTML = olhoFechado;
+
+    }
+
+});
+
+botaoConfirmarSenha.addEventListener("click", () => {
+
+    if (campoConfirmarSenha.type === "password") {
+
+        campoConfirmarSenha.type = "text";
+        botaoConfirmarSenha.innerHTML = olhoAberto;
+
+    } else {
+
+        campoConfirmarSenha.type = "password";
+        botaoConfirmarSenha.innerHTML = olhoFechado;
+
+    }
+
+});
+
 document
     .getElementById("formCadastro")
     .addEventListener("submit", async (e) => {
@@ -44,10 +111,16 @@ document
             const dados = await resposta.json();
 
             if (resposta.ok) {
+
                 alert("Cadastro realizado com sucesso!");
-                window.location.href = "../../login.html";
+
+                window.location.href =
+                    "../../login.html";
+
             } else {
+
                 alert(dados.mensagem);
+
             }
 
         } catch (erro) {
